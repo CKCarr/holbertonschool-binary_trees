40650c15a9928f1f7e7429ef7857547a4e76715f
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-04-22 19:36:51 -0700

    typo fixed

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,7 +21,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent->left != NULL)
 	{
 		left_child->left = parent->left;
-		parent->left->parent = left->child;
+		parent->left->parent = left_child;
 	}
 	parent->left = left_child;
 
